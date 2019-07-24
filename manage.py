a01489bf5dfcc1363923646b6ffb4d4b70d0ef6f
Juan Carlos Sleiman <jcsleiman17@gmail.com>
2019-07-24 15:30:06 -0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,12 +3,12 @@ import unittest
 class TestCICD(unittest.TestCase):
 
     def setUp(self):
-      print("Inicio de las pruebas")
+        print("Inicio de las pruebas")
 
     def tearDown(self):
         print("Fin de las pruebas")
 
-    def main(self):
+    def test_main(self):
 
         print("index funcional")
         self.assertEqual(render_template('index.html'))
